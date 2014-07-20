514c07099e66fbb941e4d167e3fe48db5becf3c5
hiroshixps <hiron611@gmail.com>
2014-07-20 17:21:42 +0700

    first commit 0720

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oldmatome::Application.config.secret_key_base = 'df36120541f3a977742ca0f7dde85d57bcea98ecb1d4d5a08981f623da757c709669425b5fc2b850a949b62ebf4190e8caed7332b34bbbbc556d2c50ae830ca8'
