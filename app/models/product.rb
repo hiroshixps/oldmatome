4a4d074e4c53e51427d9baa93f866a8c90b01ae6
hiroshixps <hiron611@gmail.com>
2014-09-03 01:23:11 +0700

    add category links at top

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   validates :product_name, presence: true, uniqueness: true
   def self.search(search)
     if search
-      Product.where([ 'title LIKE ?', "%#{search}%"])
+      Product.where([ 'product_name LIKE ?', "%#{search}%"])
     end
   end
 end
