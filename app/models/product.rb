55f6fe31ed44f9d21a78daf0d893644b1386e7f9
hiroshixps <hiron611@gmail.com>
2014-09-14 15:36:56 +0700

    get spec data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   validates :product_name, presence: true, uniqueness: true
+  serialize :spec, Hash
   def self.search(search)
     if search
       Product.where([ 'product_name LIKE ?', "%#{search}%"])
