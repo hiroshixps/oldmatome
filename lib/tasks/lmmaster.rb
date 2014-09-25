2b40a5b6c0ef7e7d8e9c92e236b3549bce6b30ea
hiroshixps <hiron611@gmail.com>
2014-09-25 22:51:41 +0700

    add database.yml to shared

--- lib/tasks/lmmaster.rb
+++ lib/tasks/lmmaster.rb
@@ -1,7 +1,14 @@
+require 'rubygems'
+require "#{Rails.root}/app/models/product"
+require 'bundler/setup'
+Bundler.require
 
       # 中カテゴリは取得できないので、マスタからさがして代入
+# pc.camera, keitaiカテゴリのみなので注意
 Product.all.each do |product| 
-  product.m_category = SCategory.find_by_s_category(@product.s_category).m_category.m_category 
-  product.l_category = SCategory.find_by_s_category(@product.s_category).l_category.l_category 
+  if product.s_category
+  product.m_category = SCategory.find_by_s_category(product.s_category).m_category.m_category 
+  product.l_category = SCategory.find_by_s_category(product.s_category).l_category.l_category 
   product.save
+  end
 end   
