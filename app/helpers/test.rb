84796c982ee9e7161ea65436da2ad7a8f3f57d2a
hiroshixps <hiron611@gmail.com>
2014-09-27 00:56:04 +0700

    change product fields

---
+++ app/helpers/test.rb
@@ -0,0 +1,11 @@
+
+require "open-uri"
+require "nokogiri"
+    url = "http://www.ebay.com/sch/i.html?&_nkw=mac"
+    doc = Nokogiri::HTML(open(url), nil, "UTF-8" )
+    if @ebay_detail = doc.xpath('//h3[@class="lvtitle"]/a/@href').first
+      @ebay_price = doc.xpath('//span[@class="g-b"]').first.text.gsub(/[^0-9]/,"").to_i
+      @ebay_detail = doc.xpath('//h3[@class="lvtitle"]/a/@href').first
+      p @ebay_price
+      p @ebay_detail
+    end
--- db/migrate/20140720103008_create_products.rb
+++ db/migrate/20140720103008_create_products.rb
@@ -6,9 +6,20 @@ class CreateProducts < ActiveRecord::Migration
       t.string :model
       t.string :series
       t.integer :new_price
-      t.integer :old_price
+      t.integer :amazon_price
+      t.integer :rakuten_price
+      t.integer :yahoo_price
+      t.integer :ebay_price
       t.string :img_url
       t.string :kakaku_url
+      t.string :amazon_url
+      t.string :rakuten_url
+      t.string :yahoo_url
+      t.string :ebay_url
+      t.string :amazon_list_url
+      t.string :rakuten_list_url
+      t.string :yahoo_list_url
+      t.string :ebay_list_url
       t.string :l_category
       t.string :m_category
       t.string :s_category
