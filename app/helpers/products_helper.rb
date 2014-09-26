4c1823e2e234a5ba62708d87a60f26bd77fff9a2
hiroshixps <hiron611@gmail.com>
2014-09-27 02:54:19 +0700

    add price batch

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -25,16 +25,19 @@ module ProductsHelper
   def get_amazonpc
     @keyword = @product.product_name
     Amazon::Ecs.debug = true
-    @res = Amazon::Ecs.item_search(@keyword, 
-                                   :item_page => 1, :search_index => 'All', :response_group => 'Medium')
-    logger.debug(@res.inspect)
-    if @res.items.first
-      @amazon_price = @res.items.first.get('OfferSummary/LowestUsedPrice/Amount') 
-      @amazon_detail = @res.items.first.get('DetailPageURL')
-      #    else
-      #      @amazon_recommend = @res.items.get('MoreSearchResultsUrl')
+    begin
+      @res = Amazon::Ecs.item_search(@keyword, 
+                                     :item_page => 1, :search_index => 'All', :response_group => 'Medium')
+      logger.debug(@res.inspect)
+      if @res.items.first
+        @amazon_price = @res.items.first.get('OfferSummary/LowestUsedPrice/Amount') 
+        @amazon_price2 = @res.items.first.get('OfferSummary/LowestNewPrice/Amount') 
+        @amazon_detail = @res.items.first.get('DetailPageURL')
+        #    else
+        #      @amazon_recommend = @res.items.get('MoreSearchResultsUrl')
+      end
+    rescue
     end
-    #    render :json => {:hoge => @amazon_price}
   end
 
   def get_rakutenpc
--- app/helpers/test.rb
+++ app/helpers/test.rb
@@ -1,11 +1,12 @@
 
 require "open-uri"
 require "nokogiri"
-    url = "http://www.ebay.com/sch/i.html?&_nkw=mac"
+  enc_key = URI.escape("ENVY 17-j100/CT Leap Motion SE カスタムモデル".gsub(/\[|\]/, " "))
+    url = "http://www.ebay.com/sch/i.html?&_nkw=#{enc_key}"
     doc = Nokogiri::HTML(open(url), nil, "UTF-8" )
     if @ebay_detail = doc.xpath('//h3[@class="lvtitle"]/a/@href').first
       @ebay_price = doc.xpath('//span[@class="g-b"]').first.text.gsub(/[^0-9]/,"").to_i
-      @ebay_detail = doc.xpath('//h3[@class="lvtitle"]/a/@href').first
+      @ebay_detail = doc.xpath('//h3[@class="lvtitle"]/a/@href').first.text
       p @ebay_price
       p @ebay_detail
     end
