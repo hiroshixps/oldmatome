8479b88823d77df1d9eca342f20ac17c6a78e8ee
hiroshixps <hiron611@gmail.com>
2014-09-28 15:12:46 +0700

    gix addimg

--- lib/tasks/add_img.rb
+++ lib/tasks/add_img.rb
@@ -5,11 +5,14 @@ Bundler.require
 
 
 Product.all.each do |product| 
+  begin
   if product.img_url == ""
     doc = Nokogiri::HTML(open(product.kakaku_url)) 
-    product.img_url = doc.xpath('//div[@id = "imgBox"]/img/@src').inner_text
+    product.img_url = doc.xpath('//div[@id = "imgBox"]/img/@src').inner_text == "http://img1.kakaku.k-img.com/images/productimage/l/nowprinting.gif" ? "" : doc.xpath('//div[@id = "imgBox"]/img/@src').inner_text
     product.save
   end  
+  rescue
+  end
 end
 
 
